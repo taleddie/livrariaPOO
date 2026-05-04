81023cc4811186148b265a4a8b9edce2618ff8af
Eddie <eduardaalcsantos@gmail.com>
2026-05-04 09:53:11 -0300

    Add Livro class with details and author methods

---
+++ Livro.php
@@ -0,0 +1,28 @@
+<?php
+
+class Livro
+{
+  //guardando informações
+  public function _construct(
+    private string $titulo,
+    private string $autor,
+    private int $ano,
+    private string $genero,
+    ) {}
+
+  //exibindo informações
+  public function getDetalhes(): string
+  {
+    return "{$this->titulo} ({$this->ano})";
+  }
+
+  public function getAutor(): string
+  {
+    return $this->autor;
+  }
+
+  public function getGenero(): string
+  {
+    return $this->genero;
+  }
+    
